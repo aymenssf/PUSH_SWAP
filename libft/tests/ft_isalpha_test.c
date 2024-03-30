1875aca6f37323eade55b999ca5acb6d0a241c24
aymenssf <aymen.assaf@edu.uca.ma>
2024-03-30 17:45:56 +0000

    setup project

--- libft/tests/ft_isalpha_test.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    int c = 'a';
-    printf("%d", ft_isalpha(c));
-}
--- libft/tests/ft_strlen_test.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    printf("%zu", ft_strlen("aymen"));
-}
