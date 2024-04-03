8128a1ccac23ec2cafea02e49a819ed1a912e091
Aymen ASSAF <aymen.assaf@edu.uca.ma>
2024-04-03 04:00:11 +0000

    add header

--- push_swap.h
+++ push_swap.h
@@ -31,4 +31,25 @@ int is_duplicate(int nb, t_stack **stack);
 void	all_parsing(int ac, char **av);
 void	print_error(void);
 void	free_arr(char **arr);
+
+//operations
+//push
+int ft_push(t_stack **src, t_stack **dst);
+int pa(t_stack **stack_a, t_stack **stack_b, int isprint);
+int pb(t_stack **stack_a, t_stack **stack_b, int isprint);
+//swap
+int ft_swap(t_stack **stack);
+int sa(t_stack **stack_a, int isprint);
+int sb(t_stack **stack_b, int isprint);
+int ss(t_stack **stack_a, t_stack **stack_b, int isprint);
+//rotate
+int rotate(t_stack **stack);
+int ra(t_stack **stack_a, int isprint);
+int rb(t_stack **stack_b, int isprint);
+int rr(t_stack **stack_a, t_stack **stack_b, int isprint);
+//reverse rotate
+int reverse_rotate(t_stack **stack);
+int rra(t_stack **stack_a, int isprint);
+int rrb(t_stack **stack_b, int isprint);
+int rrr(t_stack **stack_a, t_stack **stack_b, int isprint);
 #endif
