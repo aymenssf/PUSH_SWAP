4edcb2eb17384c926ffeff3cc6fea74470b94851
Aymen ASSAF <aymen.assaf@edu.uca.ma>
2024-04-23 21:18:33 +0100

    NORM :)

--- sort_algo.c
+++ sort_algo.c
@@ -6,7 +6,7 @@
 /*   By: aassaf <aassaf@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/06 22:41:29 by aassaf            #+#    #+#             */
-/*   Updated: 2024/04/22 14:44:11 by aassaf           ###   ########.fr       */
+/*   Updated: 2024/04/23 21:17:56 by aassaf           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,32 +50,48 @@ void	sort_4(t_stack **a, t_stack **b)
 	sort_4_norm(a, b);
 }
 
-void	sort_5_norm(t_stack **a, t_stack **b)
+void	sort_5_helper(t_stack **a, int min)
 {
-	if (is_sorted(*a))
-		return ;
-	pb(a, b, 1);
-	sort_4(a, b);
-	pa(a, b, 1);
+	t_stack	*tmp;
+
+	tmp = *a;
+	while (tmp)
+	{
+		if (tmp->value == min)
+		{
+			if (tmp->index == 1)
+				sa(a, 1);
+			else if (tmp->index == 2)
+			{
+				ra(a, 1);
+				ra(a, 1);
+			}
+			else if (tmp->index == 3)
+			{
+				rra(a, 1);
+				rra(a, 1);
+			}
+			else if (tmp->index == 4)
+				rra(a, 1);
+		}
+		tmp = tmp->next;
+	}
 }
 
 void	sort_5(t_stack **a, t_stack **b)
 {
 	int		min;
-	t_stack	*tmp;
 
 	if (is_sorted(*a))
 		return ;
 	stack_index(a);
 	min = find_min(*a);
-	tmp = *a;
-	while (tmp)
-	{
-		if (tmp->value == min)
-			rotation(a);
-		tmp = tmp->next;
-	}
-	sort_5_norm(a, b);
+	sort_5_helper(a, min);
+	if (is_sorted(*a))
+		return ;
+	pb(a, b, 1);
+	sort_4(a, b);
+	pa(a, b, 1);
 }
 
 void	sort_algo(t_stack **a, t_stack **b)
